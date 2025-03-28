cc610659584c6319e51d57c9c29d2a085c2b6ace
Stefan Metzmacher <metze@samba.org>
2025-03-28 11:01:23 +0100

    libquic: add a quic_handshake_ctx_get() helper
    The use of gnutls_db_get_ptr() should not be exposed
    and will change in the next commits.
    
    Signed-off-by: Stefan Metzmacher <metze@samba.org>

--- libquic/handshake.c
+++ libquic/handshake.c
@@ -55,6 +55,11 @@ struct quic_handshake_ctx {
 	gnutls_anti_replay_t quic_anti_replay;
 };
 
+static struct quic_handshake_ctx *quic_handshake_ctx_get(gnutls_session_t session)
+{
+	return gnutls_db_get_ptr(session);
+}
+
 /*
  * The caller needs to opt-in in order
  * to get log messages
@@ -337,7 +342,7 @@ static int quic_set_secret(gnutls_session_t session, gnutls_record_encryption_le
 			   const void *rx_secret, const void *tx_secret, size_t secretlen)
 {
 	gnutls_cipher_algorithm_t type  = gnutls_cipher_get(session);
-	struct quic_handshake_ctx *ctx = gnutls_db_get_ptr(session);
+	struct quic_handshake_ctx *ctx = quic_handshake_ctx_get(session);
 	struct quic_crypto_secret secret = {};
 	int sockfd, ret, len = sizeof(secret);
 
@@ -467,7 +472,7 @@ static int quic_tp_recv(gnutls_session_t session, const uint8_t *buf, size_t len
 
 static int quic_tp_send(gnutls_session_t session, gnutls_buffer_t extdata)
 {
-	struct quic_handshake_ctx *ctx = gnutls_db_get_ptr(session);
+	struct quic_handshake_ctx *ctx = quic_handshake_ctx_get(session);
 	int ret;
 
 	quic_log_notice("%s: %u", __func__,
@@ -538,7 +543,7 @@ static void quic_msg_destroy(struct quic_msg *msg)
 static int quic_msg_read(gnutls_session_t session, gnutls_record_encryption_level_t level,
 			 gnutls_handshake_description_t htype, const void *data, size_t datalen)
 {
-	struct quic_handshake_ctx *ctx = gnutls_db_get_ptr(session);
+	struct quic_handshake_ctx *ctx = quic_handshake_ctx_get(session);
 	struct quic_msg *msg;
 
 	quic_log_notice("%s: %u %u %u %zu", __func__,
